a7a87f7f5581584a5a5155a03e550a2abef0a478
Nagoluk <51786414+Nagoluk@users.noreply.github.com>
2020-03-08 17:07:40 +0200

    new post redux form

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,50 +1,44 @@
 import React from "react";
 import MyPost from "./MyPosts.module.css";
 import Post from "./post/post";
+import { Field, reduxForm } from "redux-form";
 
 
+const PostInputForm = props => {
+    return (<form className={MyPost.newpost} onSubmit={props.handleSubmit}>
+                <Field id="post" type="text" placeholder={"Що у вас нового"}  component={"input"} name={"newPostText"}/>
 
+
+                <div className="createnewpost">
+                    <button className={MyPost.button}><i className="fas fa-photo-video"></i></button>
+                    <button className={MyPost.button}><i className="fas fa-headphones-alt"></i></button>
+                    <button className={MyPost.button + " " + MyPost.send}> <i className="fas fa-paper-plane"></i></button>
+                </div>
+            </form>)
+}
+
+let ReduxFormPostInput = reduxForm({
+    form: "posts"
+})(PostInputForm)
 
 
 const MyPosts = (props) => {
 
-    console.log("Post", props);
-
-
-    let newPost = React.createRef();
-
     let addNewPost = ()=> {
-        props.addNewPost(newPost.current.value)
+        console.log("hello")
     }
-
-    let addSymbol = () =>{
-
-  
-        props.newSymbolAC(newPost.current.value);
-    }
-
-
 
     let PostsElements = props.ProfilePage.PostsData.map(currentValue => <Post message={currentValue.content} likes={currentValue.likes} rep={currentValue.rep} comm={currentValue.comm} dataSend={currentValue.dataSend}/>)
     return (
         <div className={MyPost.myposts}>
             <div className="postwrap">
-                <div className={MyPost.newpost}>
-                    <input id="post" type="text" placeholder={(props.Lang.eng) ? "What is the news?" : "Що у вас нового"} ref={newPost} value={props.ProfilePage.NewPostText} onChange={addSymbol}/>
-
-
-                    <div className="createnewpost">
-                        <button className={MyPost.button}><i className="fas fa-photo-video"></i></button>
-                        <button className={MyPost.button}><i className="fas fa-headphones-alt"></i></button>
-                        <button className={MyPost.button + " " + MyPost.send} onClick={addNewPost}><i className="fas fa-paper-plane"></i></button>
-                    </div>
-
+                <div>
+                    <ReduxFormPostInput onSubmit={addNewPost}/>
                 </div>
 
                 <div className="news">
 
                     {PostsElements}
-
 
                 </div>
             </div>
