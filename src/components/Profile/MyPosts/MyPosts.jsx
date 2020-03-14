423f7f8b24e8ecaba0d43b41f05f46373abfc8c3
Nagoluk <51786414+Nagoluk@users.noreply.github.com>
2020-03-14 16:38:59 +0200

    refactoring

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -21,8 +21,8 @@ const PostInputForm = props => {
 
 
                 <div className="createnewpost">
-                    <button className={MyPost.button}><i className="fas fa-photo-video"></i></button>
-                    <button className={MyPost.button}><i className="fas fa-headphones-alt"></i></button>
+                    <button className={MyPost.button} disabled={true}><i className="fas fa-photo-video"></i></button>
+                    <button className={MyPost.button} disabled={true}><i className="fas fa-headphones-alt"></i></button>
                     <button className={MyPost.button + " " + MyPost.send}> <i className="fas fa-paper-plane"></i></button>
                 </div>
             </form>)
@@ -34,10 +34,12 @@ let ReduxFormPostInput = reduxForm({
 
 
 const MyPosts = (props) => {
+    console.log("render")
    
 
     let addNewPost = (data)=> {
         props.addNewPostAC(data.newPostText)
+        data.resetForm();
     }
 
     let PostsElements = props.ProfilePage.PostsData.map((currentValue, index) => <Post key={index.toString()}message={currentValue.content} likes={currentValue.likes} rep={currentValue.rep} comm={currentValue.comm} dataSend={currentValue.dataSend}/>)
