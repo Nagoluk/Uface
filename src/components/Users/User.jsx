8d2341b9ac119087e2a482eedce17ac5d3166b7c
Nagoluk <51786414+Nagoluk@users.noreply.github.com>
2020-03-15 20:12:17 +0200

    Users refactor

---
+++ src/components/Users/User.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import UsersStlyes from "./Users.module.css";
+import Avatar from "../../img/Profile/avatar.png";
+import {NavLink} from "react-router-dom";
+
+
+let User = props => {
+    return (<div className={UsersStlyes.item} key={props.index.toString()}>
+        <div className={UsersStlyes.header}>
+            <div className={UsersStlyes.avatar}>
+                <img
+                    src={(props.user.photos.small !== null) ? props.user.photos.small : Avatar}
+                    alt="avatar"/>
+            </div>
+        </div>
+
+        <div className={UsersStlyes.content}>
+            <div className={UsersStlyes.name}>
+                <NavLink to={"/profile/" + props.user.id}><h2>{props.user.name}</h2></NavLink>
+            </div>
+
+            <div className={UsersStlyes.status}>{props.user.status}</div>
+
+            <div className={UsersStlyes.location}>
+                <span className={UsersStlyes.country}> Minsk </span>
+                <span className={UsersStlyes.city}>Belarus </span>
+            </div>
+        </div>
+
+        <div className={UsersStlyes.buttons}>
+            {props.user.followed ?
+                <button disabled={props.followProcces.some(item => item === props.user.id)}
+                        className={UsersStlyes.follower}
+                        onClick={() => {
+                            props.unfollowThunkCreator(props.user.id)
+                        }}>Unfollow</button> :
+                <button disabled={props.followProcces.some(item => item === props.user.id)}
+                        onClick={() => {
+                        props.followThunkCreator(props.user.id)
+                }}>follow</button>
+            }
+        </div>
+
+    </div>)
+}
+
+export default User;
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import UsersStlyes from './Users.module.css';
-import {NavLink} from "react-router-dom";
-import Avatar from  "../../img/Profile/avatar.png";
-
+import User from "./User";
 
 
 let Users = (props) => {
@@ -12,58 +10,20 @@ let Users = (props) => {
         pages.push(i);
     }
 
-    return <div>
-        <div className={UsersStlyes.settingPanel}>
-            {pages.map((p, index) => {
-                return <button onClick={() => {
-                    props.onPageChange(p)
-                }} className={(props.currentPage === p && UsersStlyes.active).toString() } key={index.toString()}>{p}</button>
-            })}
-          
-        </div>
-        <div className={UsersStlyes.itemWrap}>
-            {props.users.map((u, index) => {
-                return <div className={UsersStlyes.item} key={index.toString()}>
-                    <div className={UsersStlyes.header}>
-                        <div className={UsersStlyes.avatar}>
-                            <img
-                                src={(u.photos.small !== null) ? u.photos.small : Avatar}
-                                alt="avatar"/>
-                        </div>
-
-                    </div>
-
-                    <div className={UsersStlyes.content}>
-
-                        <div className={UsersStlyes.name}>
-                            <NavLink to={"/profile/" + u.id}><h2>{u.name}</h2></NavLink>
-                        </div>
-                        <div className={UsersStlyes.status}>{u.status}</div>
-                        <div className={UsersStlyes.location}>
-                            <span className={UsersStlyes.country}> Minsk </span>
-                            <span className={UsersStlyes.city}>Belarus </span>
-                        </div>
-
-
-                    </div>
-
-                    <div className={UsersStlyes.buttons}>
-                        {u.followed ?
-                            <button disabled = {props.followProcces.some(item => item === u.id)} className={UsersStlyes.follower} onClick={() => {
-                                props.unfollowThunkCreator(u.id)
-                            }}>Unfollow</button> :
-                            <button disabled = {props.followProcces.some(item => item === u.id)} onClick={() => {
-                                props.followThunkCreator(u.id)
-
-                            }}>follow</button>
-                        }
-                    </div>
-
+    return (<div>
+                <div className={UsersStlyes.settingPanel}>
+                    {pages.map((p, index) => {
+                        return <button onClick={() => {
+                            props.onPageChange(p)
+                        }} className={(props.currentPage === p && UsersStlyes.active).toString()}
+                                       key={index.toString()}>{p}</button>
+                    })}
                 </div>
-            })
-            }
-        </div>
-    </div>
+
+                <div className={UsersStlyes.itemWrap}>
+                    {props.users.map((user, index) => <User user={user} index={index} {...props}/>)}
+                </div>
+             </div>)
 
 }
 
