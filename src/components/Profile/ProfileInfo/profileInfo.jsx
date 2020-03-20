f10855bd3d4b361d05ef815fd282f09b38b14dc2
Nagoluk <nagoluk1626@gmail.com>
2020-03-20 14:12:05 +0200

    avatar

--- src/components/Profile/ProfileInfo/profileInfo.jsx
+++ src/components/Profile/ProfileInfo/profileInfo.jsx
@@ -21,30 +21,63 @@ const ProfileInfo = (props) => {
                 <div className={p.information}>
 
 
-                    <div >
-                        <img src={props.profile.profile.photos.large || profileAvatar} className={p.avatar} alt="Avatar"/>
+                {amI && <div className={p.avatarWrap}>
+                        <div className={p.changeAvatarContainer}>
+                            <img src={props.profile.profile.photos.large || profileAvatar} className={p.avatar} alt="Avatar"/>
+                            <label  htmlFor="avatar" className={p.changeAvatar}>
+                               <div className={p.AvatarControl}>
+                                            <i className="fas fa-upload"></i>
+                                            <input type="file" id="avatar" className={p.Avatar} onChange={uploadPhoto}/>
+                                </div>
+                            </label>
+                        </div>
                     </div>
+                }
 
-                    {amI && <div className={p.AvatarControl}>
-                        <label htmlFor="avatar"><i className="fas fa-upload"></i>Upload avatar</label>
-                        <input type="file" id="avatar" className={p.Avatar} onChange={uploadPhoto}/>
-                    </div>}
+                {!amI && <div className={p.avatarWrap} >
+                       
+                    <img src={props.profile.profile.photos.large || profileAvatar} 
+                    className={p.avatar} style={{border: "3px solid #fff"}}
+                    alt="Avatar"/>
+                            
+                </div>
+                }
+
+                    
 
                     <div className={p.info}>
                         <h2>{props.profile.profile.fullName}</h2>
                         <ul>
-                            <li><span className={p.infoItem}>{(props.lang.eng) ? "Status:" : "Статус"} </span>
-                                <Status status={props.status || props.profile.aboutMe || "No status"}
+                            <li>
+                                <span className={p.infoItem}>Status: </span>
+                                <Status status={props.status || "No status"}
                                         updateStatusThunkCreator={props.updateStatusThunkCreator}
                                         amI={amI}
                                 />
                             </li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
+                            <li><span className={p.infoItem}>About me: </span> {props.profile.aboutMe || "No about me"}</li>
+                          
                         </ul>
                     </div>
 
+
+                    
+
+                </div>
+
+
+                <div className={p.information + " " + p.contacts}>
+                    <ul>
+                        {/* <li><p>Contacts: </p></li> */}
+                        <li><i className="fab fa-facebook"></i></li>
+                        <li><i className="fab fa-internet-explorer"></i></li>
+                        <li><i className="fab fa-vk"></i></li>
+                        <li><i className="fab fa-twitter"></i></li>
+                        <li><i className="fab fa-instagram"></i></li>
+                        <li><i className="fab fa-youtube"></i></li>
+                        <li><i className="fab fa-github"></i></li>
+                        <li><i className="fab fa-linkedin-in"></i></li>
+                    </ul>
                 </div>
 
             </div>
