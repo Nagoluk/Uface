29e05d1b65a4d31f8e73eb71423d7eb8360ea732
Dmitriy Naholiuk <nagoluk1626@gmail.com>
2020-05-21 19:35:50 +0300

    Refactor(UsersSelectors): to TS

---
+++ src/Redux/usersSelectors.ts
@@ -0,0 +1,17 @@
+import {createSelector} from "reselect";
+import {AppStateType} from "./stateRedux";
+
+
+const getUsersSelector = (state: AppStateType) => state.UsersReducer.users;
+
+export const getUsersSuperSelector = createSelector(getUsersSelector, (users) => users.filter(i => true))
+//selector with complicated logic .... Z >Z >Z 
+
+
+export const getPageSizeSelector = (state: AppStateType) => state.UsersReducer.pageSize;
+export const getTototalUsersCountSelector = (state: AppStateType) => state.UsersReducer.totalUsersCount;
+export const getCurrentPageSelector = (state: AppStateType) => state.UsersReducer.currentPage;
+export const getIsFetchingSelector = (state: AppStateType) => state.UsersReducer.isFetching;
+export const getPagePagitator = (state: AppStateType) => state.UsersReducer.currentPagePagitator;
+export const getFollowProccesSelector = (state: AppStateType) => state.UsersReducer.followProcces;
+export const getIsLoginedSelector = (state: AppStateType) => state.LoginReducer.isLogined;
