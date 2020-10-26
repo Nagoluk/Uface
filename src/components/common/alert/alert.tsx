afd658edf9a2a578f009c1405349b13af61ed2bc
Naholiuk Dmitriy <nagoluk1626@gmail.com>
2020-10-26 11:41:36 +0200

    Refactor(alert): to ts

--- src/components/common/alert/alert.tsx
+++ src/components/common/alert/alert.tsx
@@ -2,8 +2,13 @@ import React from "react";
 
 import AlertStyles from "./alert.module.css";
 
+type AlertPropsType = {
+    deleteMessage: (param: string) => void
+    setDeleteMessageAlert: (param1: boolean, param2: null) => void
+    deleteMessageId: string
 
-let Alert = props => {
+}
+let Alert: React.FC<AlertPropsType> = props => {
     return (<div className={AlertStyles.Wrap}>
                 <h2>Do you really want to delete this message?</h2>
                 <button className={AlertStyles.Yes} onClick={() => props.deleteMessage(props.deleteMessageId)}>Yes</button>
